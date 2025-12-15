38b424e73ceb4126a831fe531eddf472592d5914
Emre Kurşun <kursunemre01@gmail.com>
2025-12-15 19:54:28 +0300

    test(domain/index): extend and refactor tests for `Index` operations
    Introduces new unit tests for `AddOrUpdateEntry` and `RemoveEntry` methods, ensuring proper handling of existing and non-existent paths. Refactors `AddEntry` tests to use `createTestEntry` for cleaner test setup.

--- domain/index_test.go
+++ domain/index_test.go
@@ -8,6 +8,12 @@ import (
 	"github.com/stretchr/testify/assert"
 )
 
+var hashList = []string{
+	"625786dc2c6ad841446f9394fd439ce53bf6442354d0b0d095b97f6b12499419",
+	"4c64cbb574119e68e31489ac8f38350b43f3ac75361a472563dd468f70b77652",
+	"658574d668dec4d0bb7145fed0ab97a3c348f0db21389e65416c03557ea48c3d",
+}
+
 func TestNewEmptyIndex(t *testing.T) {
 	index := NewEmptyIndex()
 
@@ -20,18 +26,10 @@ func TestNewEmptyIndex(t *testing.T) {
 
 	assert.Equal(t, "", index.Checksum, "checksum should be empty string")
 }
+
 func TestAddEntry_SingleEntry(t *testing.T) {
 	index := NewEmptyIndex()
-	entry := NewIndexEntry(
-		"test.txt",
-		"abc123def456",
-		100,
-		uint32(RegularFile),
-		0, 0, 0, 0,
-		8,
-		time.Now(),
-		time.Now(),
-	)
+	entry := createTestEntry("a.txt", hashList[0])
 
 	index.AddEntry(entry)
 	assert.Equal(t, 1, len(index.Entries), "entries slice should have length 1")
@@ -40,28 +38,11 @@ func TestAddEntry_SingleEntry(t *testing.T) {
 	addedEntry := index.Entries[0]
 	assert.Equal(t, addedEntry, entry)
 }
+
 func TestAddEntry_MultipleEntries(t *testing.T) {
 	index := NewEmptyIndex()
-	firstEntry := NewIndexEntry(
-		"test1.txt",
-		"123456789",
-		100,
-		uint32(RegularFile),
-		0, 0, 0, 0,
-		8,
-		time.Now(),
-		time.Now(),
-	)
-	secondEntry := NewIndexEntry(
-		"test2.txt",
-		"123654789",
-		100,
-		uint32(RegularFile),
-		0, 0, 0, 0,
-		8,
-		time.Now(),
-		time.Now(),
-	)
+	firstEntry := createTestEntry("a.txt", hashList[0])
+	secondEntry := createTestEntry("b.txt", hashList[1])
 
 	index.AddEntry(firstEntry)
 	index.AddEntry(secondEntry)
@@ -76,10 +57,58 @@ func TestAddEntry_MultipleEntries(t *testing.T) {
 	assert.Equal(t, secondEntry, secondAddedEntry)
 }
 
-// func TestAddOrUpdateEntry_NewPath(t *testing.T)
-// func TestAddOrUpdateEntry_ExistingPath(t *testing.T)
-// func TestRemoveEntry_ExistingPath(t *testing.T)
-// func TestRemoveEntry_NonExistentPath(t *testing.T)
+func TestAddOrUpdateEntry_ExistingPath(t *testing.T) {
+	index := NewEmptyIndex()
+	entry := createTestEntry("a.txt", hashList[0])
+
+	index.AddEntry(entry)
+	assert.Equal(t, entry, index.Entries[0])
+
+	entry.Hash = hashList[1]
+
+	index.AddOrUpdateEntry(entry)
+	assert.Equal(t, entry, index.Entries[0])
+}
+
+func TestRemoveEntry_ExistingPath(t *testing.T) {
+	index := NewEmptyIndex()
+	entry := createTestEntry("a.txt", hashList[0])
+
+	index.AddEntry(entry)
+	assert.Equal(t, 1, len(index.Entries))
+	assert.Equal(t, index.Header.NumEntries, uint32(len(index.Entries)))
+
+	index.RemoveEntry(entry.Path)
+	assert.Equal(t, 0, len(index.Entries))
+	assert.Equal(t, index.Header.NumEntries, uint32(len(index.Entries)))
+}
+
+func TestRemoveEntry_NonExistentPath(t *testing.T) {
+	index := NewEmptyIndex()
+	entry := createTestEntry("a.txt", hashList[0])
+
+	index.AddEntry(entry)
+	assert.Equal(t, 1, len(index.Entries))
+	assert.Equal(t, index.Header.NumEntries, uint32(len(index.Entries)))
+
+	index.RemoveEntry("nonexistent path")
+	assert.Equal(t, 1, len(index.Entries))
+	assert.Equal(t, index.Header.NumEntries, uint32(len(index.Entries)))
+}
+
 // func TestSerialize_EmptyIndex(t *testing.T)
 // func TestDeserialize_InvalidSignature(t *testing.T)
 // func TestDeserialize_ChecksumMismatch(t *testing.T)
+
+func createTestEntry(path, hash string) *IndexEntry {
+	return NewIndexEntry(
+		path,
+		hash,
+		100,
+		uint32(RegularFile),
+		0, 0, 0, 0,
+		uint16(len(path)),
+		time.Now(),
+		time.Now(),
+	)
+}
