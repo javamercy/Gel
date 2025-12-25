6850aea9f69945d1f0aa219721a8934467644690
Emre Kurşun <kursunemre01@gmail.com>
2025-12-25 16:05:28 +0300

    feat(util): add function to find null byte index in byte slice

---
+++ core/util/helper.go
@@ -0,0 +1,12 @@
+package util
+
+import "Gel/core/constant"
+
+func FindNullByteIndex(data []byte) int {
+	for i, b := range data {
+		if b == constant.NullByte {
+			return i
+		}
+	}
+	return -1
+}
